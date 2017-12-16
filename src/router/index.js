d9d093c86e271516306b62ccb3172d448f718c00
xurui <xurui@wzdai.com>
2017-12-16 13:36:30 +0800

    修改冲突

--- src/router/index.js
+++ src/router/index.js
@@ -5,12 +5,8 @@ Vue.use(Router)
 //异步路由
 const theme14 = r => require.ensure([], () => r(require('../views/theme14/theme14')), 'theme14')
 const agreement = r => require.ensure([], () => r(require('../views/agreement')), 'agreement')
-<<<<<<< HEAD
 const loadingGame = r => require.ensure([], () => r(require('../views/loading/loadingGame')), 'loadingGame')
-=======
-const loading = r => require.ensure([], () => r(require('../views/loading/loading')), 'loading')
 const home = r => require.ensure([], () => r(require('../views/home/home')), 'home')
->>>>>>> 28e70047e56cb226039ef22a761efb3ce4059e34
 export default new Router({
   routes: [
     {
@@ -28,20 +24,14 @@ export default new Router({
       component: agreement
     },
     {
-<<<<<<< HEAD
       path: '/loading/loadingGame',
       name: 'loadingGame',
       component: loadingGame
-=======
-      path: '/loading',
-      name: 'loading',
-      component: loading
     },
     {
       path: '/home',
       name: 'home',
       component: home
->>>>>>> 28e70047e56cb226039ef22a761efb3ce4059e34
     }
 
   ]
--- src/views/theme14/theme14.vue
+++ src/views/theme14/theme14.vue
@@ -1,15 +1,9 @@
 <template>
   <div class="theme14">
     <img src="../../images/bg_test.png" class="banner">
-
     <div class="page-modal" v-show="showModal">
-<<<<<<< HEAD
         <router-link to="/agreement">首页</router-link>
         <router-link to="/loading/loadingGame">loading</router-link>
-=======
-        <router-link to="/home">首页</router-link>
-        <router-link to="/loading">loading</router-link>
->>>>>>> 28e70047e56cb226039ef22a761efb3ce4059e34
         <router-link to="/agreement">页面1</router-link>
         <router-link to="/agreement">页面1</router-link>
         <router-link to="/agreement">页面1</router-link>
