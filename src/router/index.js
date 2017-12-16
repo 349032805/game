fe4eb0445bdde7838b325546f63327ff7d741c89
李逸威 <liyiwei@wzdai.com>
2017-12-16 15:05:35 +0800

    shop

--- src/router/index.js
+++ src/router/index.js
@@ -5,14 +5,11 @@ Vue.use(Router)
 //异步路由
 const theme14 = r => require.ensure([], () => r(require('../views/theme14/theme14')), 'theme14')
 const agreement = r => require.ensure([], () => r(require('../views/agreement')), 'agreement')
-<<<<<<< HEAD
-const loading = r => require.ensure([], () => r(require('../views/loading/loading')), 'loading')
+
 const shop = r => require.ensure([], () => r(require('../views/shop/shop')), 'shop')
-=======
 const loadingGame = r => require.ensure([], () => r(require('../views/loading/loadingGame')), 'loadingGame')
 const home = r => require.ensure([], () => r(require('../views/home/home')), 'home')
 const loadingYyl = r => require.ensure([], () => r(require('../views/loading/loadingYyl')), 'loadingYyl')
->>>>>>> 7679eb22d4344d073d481bac2c8e8a538bbf9d50
 export default new Router({
   linkActiveClass: "active",
   routes: [
@@ -31,17 +28,11 @@ export default new Router({
       component: agreement
     },
     {
-<<<<<<< HEAD
-      path: '/loading',
-      name: 'loading',
-      component: loading
+      path: '/shop',
+      name: 'shop',
+      component: shop
     },
     {
-      path:'/shop',
-      name:'shop',
-      component:shop
-    }
-=======
       path: '/loading/loadingGame',
       name: 'loadingGame',
       component: loadingGame
@@ -50,12 +41,10 @@ export default new Router({
       path: '/home',
       name: 'home',
       component: home
-    },{
+    }, {
       path: '/loading/loadingYyl',
       name: 'loadingYyl',
       component: loadingYyl
     },
-
->>>>>>> 7679eb22d4344d073d481bac2c8e8a538bbf9d50
   ]
 })
